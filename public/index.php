e6e6eca8cb7bb647ca5f8706a94809a8e949d5f8
Ju/TroisYaourts <julien@troisyaourts.com>
2020-05-18 14:54:07 +0200

    cleanup autoloader

--- public/index.php
+++ public/index.php
@@ -6,38 +6,6 @@
 
   require_once("vendor/autoload.php");
 
-  $insee = new \Iso9005\Geography\Norms\ZipCode();
-
-  // //on initialise l'autoloader
-  // function initiation_autoload($classname){
-
-  //   // Iso9005\Geography\Norms\InseeCode
-  //   $components = explode('\\',$classname);
-  //   //["Iso9005","Geography","Norms", "InseeCode"]
-  //   $rootNS = reset($components);
-  //   //"Iso9005"
-  //   $simpleClassName = end($components);
-  //   //"InseeCode"
-
-  //   if($rootNS == "Iso9005"){
-  //     //follow psr4 rules
-  //     require_once("Library/ISO-9005/".$simpleClassName.".php");
-  //   }
-
-  //   if($rootNS == "Zend"){
-  //     //"Zend_Services_Agents_SOAP_WSDLAgent"
-  //     //follow psr4 rules
-  //     $path = str_replace("_", "/", $classname);
-  //     require_once("ZendLib/".$path.".php");
-  //   }
-    
-  //   // else follow psr0 rules
-  //   $path = str_replace("\\", "/", $classname);
-  //   require_once($path.".php");
-
-  // }
-  // spl_autoload_register("initiation_autoload");
-
   //on initialise la connexion à la bdd
   require_once("config/secret.php");
 
